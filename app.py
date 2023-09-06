d525172d116a431e132559f9cf2037c3247cbcf3
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2023-09-06 13:15:36 +0200

    Improved logging

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, abort, request
+import logging
+from uuid import uuid4
 import os
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger("latex")
 
 @app.route("/", methods=["POST"])
 def handle_request():
-    print("Request received")
+    uuid = uuid4()
+    log.info("Request received with ID %s", uuid)
     request_body = request.data.decode("utf-8")
 
-    print("Request body: " + request_body)
+    try:
+        with open(f"{uuid}.tex", "w") as f:
+            f.write(request_body)
+    except:
+        log.error('Failed to write request body to temporary file.')
+        abort(400)
 
-    with open("test.tex", "w") as f:
-        f.write(request_body)
+    log.info("TeX file saved. Starting LaTeX compilation.")
+    os.system(f"latexmk -pdf {uuid}.tex")
 
-    print("TeX file saved. Starting LaTeX compilation.")
-    os.system("latexmk -pdf test.tex")
-
-    print("Compilation finished. Sending response.")
-    with open("test.pdf", "rb") as f:
+    log.info("Compilation finished. Sending response.")
+    with open(f"{uuid}.pdf", "rb") as f:
         pdf = f.read()
 
     response = Response(pdf, content_type="application/pdf")
     response.headers.set("Content-Disposition", "attachment", filename="test.pdf")
 
-    # TODO: this command should run after sending the response to clean up all the aux files.
-    # TODO: come up with a better/dynamic file naming system
-    # os.system("latexmk -C test.tex")
+    # TODO: Celery should run a clean-up script to remove additional build artifacts.
+    # os.system("latexmk -C {uuid}.tex")
 
     return response
 
 
 if __name__ == "__main__":
+    log.info("Starting LaTeX Service!")
     app.run()
